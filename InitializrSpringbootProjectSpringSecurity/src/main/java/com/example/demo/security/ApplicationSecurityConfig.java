b8b542092780c9ae7a3487195f0f0203d178b58b
Alvaro Pereira do Nascimento <33030108+AlvaroPereiradoNascimento@users.noreply.github.com>
2020-04-07 07:42:01 -0300

    Inserido usuário Tom com papel admintrainee
    Usuário com permissão somente para leitura de acesso.

--- InitializrSpringbootProjectSpringSecurity/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ InitializrSpringbootProjectSpringSecurity/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -8,6 +8,7 @@ package com.example.demo.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -35,9 +36,13 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/","/index","/css/**","/js/**").permitAll()
-                .antMatchers("/api/**").hasRole(ApplicationUserRole.STUDENT.name())
+                .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.name())
+                .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.name())
+                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.name())
+                .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ApplicationUserRole.ADMIN.name(),ApplicationUserRole.ADMINTRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -54,16 +59,22 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter{
                 .roles(ApplicationUserRole.STUDENT.name())
                 .build(); //ROLE_STUDENT
         
-       UserDetails Linda = User.builder()
+       UserDetails LindaUser = User.builder()
                 .username("Linda")
                 .password(passowordEncoder.encode("password123"))
                 .roles(ApplicationUserRole.ADMIN.name())
                 .build();
+       
+       UserDetails TomUser = User.builder()
+                .username("Tom")
+                .password(passowordEncoder.encode("password123"))
+                .roles(ApplicationUserRole.ADMINTRAINEE.name())
+                .build();
         return new InMemoryUserDetailsManager(
                 annaSmithUser,
-                Linda
+                LindaUser,
+                TomUser
         );
     }
     
 }
-
