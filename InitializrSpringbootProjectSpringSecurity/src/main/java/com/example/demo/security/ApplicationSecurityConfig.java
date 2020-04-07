d8c00d3dcb7f43f13410eccc78c77e2c57c27506
Alvaro Pereira do Nascimento <33030108+AlvaroPereiradoNascimento@users.noreply.github.com>
2020-04-07 04:59:39 -0300

    Inserido antMatcher com permissão por Role
    .antMatchers("/api/**").hasRole(ApplicationUserRole.STUDENT.name())

--- InitializrSpringbootProjectSpringSecurity/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ InitializrSpringbootProjectSpringSecurity/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -5,10 +5,17 @@
  */
 package com.example.demo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  *
@@ -18,16 +25,45 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter{
     
+    private final PasswordEncoder passowordEncoder;
+    
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passowordEncoder){
+        this.passowordEncoder = passowordEncoder;
+    }
     
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                 .antMatchers("/","/index","/css/**","/js/**").permitAll()
+                .antMatchers("/api/**").hasRole(ApplicationUserRole.STUDENT.name())
                 .anyRequest()
                 .authenticated()
                 .and()
                 .httpBasic();     
     }
     
+    
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService(){
+        UserDetails annaSmithUser = User.builder() 
+                .username("annasmith")
+                .password(passowordEncoder.encode("password"))
+                .roles(ApplicationUserRole.STUDENT.name())
+                .build(); //ROLE_STUDENT
+        
+       UserDetails Linda = User.builder()
+                .username("Linda")
+                .password(passowordEncoder.encode("password123"))
+                .roles(ApplicationUserRole.ADMIN.name())
+                .build();
+        return new InMemoryUserDetailsManager(
+                annaSmithUser,
+                Linda
+        );
+    }
+    
 }
+
