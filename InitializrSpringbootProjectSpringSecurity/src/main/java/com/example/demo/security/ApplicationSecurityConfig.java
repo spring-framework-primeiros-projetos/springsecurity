8de12927d5b40056d92672b81344c701e18c6bdf
Alvaro Pereira do Nascimento <alvaro.nascimentoapn@gmail.com>
2020-04-08 01:51:58 -0300

    Remember me

--- InitializrSpringbootProjectSpringSecurity/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ InitializrSpringbootProjectSpringSecurity/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -5,6 +5,7 @@
  */
 package com.example.demo.security;
 
+import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -51,6 +52,10 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter{
                 .formLogin()
                 .loginPage("/login").permitAll()
                 .defaultSuccessUrl("/courses",true)
+                .and()
+                .rememberMe()
+                    .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
+                    .key("somethingverysecured")
                 ;     
     }
     
