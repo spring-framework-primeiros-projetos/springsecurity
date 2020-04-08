ee748bc5fb16153e001df1ee0c0d70a8208a75a9
Alvaro Pereira do Nascimento <alvaro.nascimentoapn@gmail.com>
2020-04-08 02:24:39 -0300

    Configurando o Logout

--- InitializrSpringbootProjectSpringSecurity/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ InitializrSpringbootProjectSpringSecurity/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /**
  *
@@ -56,6 +57,14 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter{
                 .rememberMe()
                     .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
                     .key("somethingverysecured")
+                .and()
+                .logout()
+                    .logoutUrl("/logout")
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout","GET"))//para crsf ativo deve utilizar o post
+                    .clearAuthentication(true)
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID","remember-me")
+                    .logoutSuccessUrl("/login")
                 ;     
     }
     
